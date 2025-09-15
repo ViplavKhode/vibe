cbefd89f29fb893febfc52bd5d37a88a08e3a0ad
ViplavKhode <yesviplavkhode@gmail.com>
2025-09-14 20:53:31 -0400

    setup e2b sandboxes and run it

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,18 @@
 import { gemini, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
+import { Sandbox} from "@e2b/code-interpreter";
+import { getSandbox } from "./util";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-is", async () => {
+      const sandbox = await Sandbox.create("vibe-nextjs-test-8788");
+      
+      return sandbox.sandboxId;
+    });
+
 
     const codeAgent = createAgent({
       name: "codeAgent",
@@ -16,6 +24,13 @@ export const helloWorld = inngest.createFunction(
       `Write the following snippet: ${event.data.value}`,
       );
       console.log("Output: "+ output);
-      return { output };
+
+      const sandboxUrl = await step.run("get-sandbox-url", async () => {
+        const sandbox = await getSandbox(sandboxId);
+        const host = sandbox.getHost(3000)
+        return `https://${host}`;
+      });
+
+      return { output, sandboxUrl };
     },
 );
