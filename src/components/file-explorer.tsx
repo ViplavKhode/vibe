c403cb5de59fed01001ffa2c71e77d9b66be2825
ViplavKhode <yesviplavkhode@gmail.com>
2025-09-25 01:38:17 -0400

    complete the develeopement of code view

--- src/components/file-explorer.tsx
+++ src/components/file-explorer.tsx
@@ -19,6 +19,10 @@ import {
     BreadcrumbSeparator,
     BreadcrumbEllipsis
  } from "@/components/ui/breadcrumb";
+import { convertFilesToTreeItems } from "@/lib/utils";
+import { TreeView } from "./tree-view";
+import { se } from "date-fns/locale";
+import { Elsie_Swash_Caps } from "next/font/google";
 
  type FileCollection = {[path: string] : string};
 
@@ -28,6 +32,73 @@ import {
     return extension || "text";
  }
 
+ interface FileBreadcrumbProps{
+    filePath: string;
+ }
+
+const FileBreadcrumb = ({filePath}: FileBreadcrumbProps) => {
+    const pathSegments = filePath.split("/");
+    const maxSegment = 4;
+
+    const renderBreadcrumItems = ()=>{
+        if(pathSegments.length <= maxSegment){
+            return pathSegments.map((segment, index)=>{
+                const isLast = index === pathSegments.length - 1;
+                
+                return(
+                    <Fragment key={index}>
+                        <BreadcrumbItem>
+                            {isLast ? (
+                                <BreadcrumbPage className="font-medium">
+                                    {segment}
+                                </BreadcrumbPage>
+                            ): (
+                                <span className="text-muted-foreground"> 
+                                    {segment}
+                                </span>
+                            )}
+                        </BreadcrumbItem>
+                        {!isLast && <BreadcrumbSeparator />}
+                    </Fragment>
+                )
+            })
+        }
+        else{
+            const firstSegement = pathSegments[0];
+            const lastSegment = pathSegments[pathSegments.length - 1];
+
+            return(
+                <>
+                    <BreadcrumbItem>
+                        <span className="text-muted-foreground">
+                            {firstSegement}
+                        </span>
+                        <BreadcrumbSeparator />
+                        <BreadcrumbItem>
+                            <BreadcrumbEllipsis/>
+                        </BreadcrumbItem>
+                        <BreadcrumbSeparator/>
+                        <BreadcrumbItem>
+                            <BreadcrumbPage className="font-medium">
+                                {lastSegment}
+                            </BreadcrumbPage>
+                        </BreadcrumbItem>
+                    </BreadcrumbItem>
+                </>
+            )
+        }
+    };
+
+    return(
+        <Breadcrumb>
+            <BreadcrumbList>
+                {renderBreadcrumItems()}
+            </BreadcrumbList>
+        </Breadcrumb>
+    )
+
+}
+
 interface FileExplorerProps{
     files: FileCollection;
 }
@@ -36,15 +107,44 @@ export const FileExplorer = ({
     files,
  }: FileExplorerProps) => {
 
+    const[copied, setCopied] = useState(false);
+
     const [selectedFile, setSelectedFile] = useState<string | null>(()=>{
         const fileKeys = Object.keys(files);
         return fileKeys.length > 0 ? fileKeys[0] : null    
     });
 
+    const treeData = useMemo(() => {
+        return convertFilesToTreeItems(files);
+    }, [files]);
+
+    const handleFileSelect = useCallback((
+        filePath: string
+    ) => {
+        if(files[filePath]){
+            setSelectedFile(filePath);
+        }
+    }, [files]);
+
+     const handleCopy = useCallback(() => {
+        if(selectedFile){
+            navigator.clipboard.writeText(files[selectedFile]);
+            setCopied(true);
+            setTimeout(()=>{
+                setCopied(false);
+            }, 2000);
+        }
+    }, [selectedFile, files]);
+
+
     return(
         <ResizablePanelGroup direction="horizontal">
             <ResizablePanel defaultSize={30} minSize={30} className="bg-sidebar">
-                <p> TODO: Tree View</p>
+                <TreeView
+                    data={treeData}
+                    value={selectedFile}
+                    onSelect={handleFileSelect}
+                />
             </ResizablePanel>
             <ResizableHandle className="hover:bg-primary transition-colors"/>
 
@@ -52,15 +152,18 @@ export const FileExplorer = ({
                 {selectedFile && files[selectedFile] ? (
                     <div className="h-full w-full flex flex-col">
                         <div className="border-b bg-sidebar px-4 py-2 flex justify-between items-center gap-x-2">
+
+                            <FileBreadcrumb filePath={selectedFile}/>
+
                             <Hint text="Copy to clipboard" side="bottom">
                                 <Button
                                     variant="outline"
                                     size="icon"
                                     className="ml-auto"
-                                    onClick={()=>{}}
-                                    disabled={false}
+                                    onClick={handleCopy}
+                                    disabled={copied}
                                 >
-                                    <CopyIcon />
+                                {copied ? <CopyCheckIcon/> : <CopyIcon />}
                                 </Button>
                             </Hint>
                         </div>
